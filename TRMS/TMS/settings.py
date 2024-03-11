b3f3cdb395027943f197bd7ef6673f03854746e5
MerVitz <amakaluvitalis202@gmail.com>
2024-03-11 20:21:28 +0300

    Static files sotted; the css. the js, the images,

--- TRMS/TMS/settings.py
+++ TRMS/TMS/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
+    'TMSapp',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myproject.urls'
+ROOT_URLCONF = 'TMS.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myproject.wsgi.application'
+WSGI_APPLICATION = 'TMS.wsgi.application'
 
 
 # Database
