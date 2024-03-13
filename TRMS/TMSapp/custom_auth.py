2d5a642417dc83755b5a6c7e58ee7cc4722d7140
Amakalu Vitalis <amakaluvitalis202@gmail.com>
2024-03-13 20:39:25 +0300

    profile,users,supperuser,driverspage

--- TRMS/TMSapp/custom_auth.py
+++ TRMS/TMSapp/custom_auth.py
@@ -3,11 +3,15 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.backends import ModelBackend
 from .models import Driver
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
 
 class DriverAuthBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None):
+        User = get_user_model()
         try:
-            driver = Driver.objects.get(username=username, licence_number=password)
-            return driver.user  # Assuming you have a ForeignKey to the User model in your Driver model
-        except Driver.DoesNotExist:
+            user = User.objects.get(username=username)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
             return None
